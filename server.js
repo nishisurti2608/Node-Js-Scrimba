02d2bba8e178d19f8edd7e64a3be842c36f5a635
Nishi Surti <nishisurti@Nishis-MacBook-Pro.local>
2026-06-08 17:44:23 +0530

    create http server

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log("We are awesome")
+/* http modules allows data to be transferred over the http protocol
+create servers 
+handle requests from client 
+provide responses to those request  */
+
+
+
+import http from 'node:http'
+
+const PORT = 3000;
+
+const server = http.createServer((req,res)=>{
+
+    res.end("Response Ended Successfully!")
+})
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
